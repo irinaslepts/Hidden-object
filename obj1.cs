1382e512c6fb04ef41e26c4d8ebe922db5b2be48
irinaslepts <73934334+irinaslepts@users.noreply.github.com>
2021-02-08 15:59:39 +0900

    Add files via upload

---
+++ obj1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class obj1 : MonoBehaviour
+{
+    static int score = 0;
+    Transform obj_transform;
+    public GameObject text;
+    Transform text_transform;
+    MeshRenderer display;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj_transform = GetComponent<Transform>();
+        text_transform = text.GetComponent<Transform>();
+        display = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown(){
+        score = score + 1;
+        print(score);
+        if (score == 4){
+            SceneManager.LoadScene("story2");
+        }
+        
+        //capsule_transform.Translate(-2f,0,0);
+        Destroy(text);
+        display.enabled = false;
+    }
+}
